6027c88e37806f8a40970d4b573713bbe3ae020e
ymekuria <ymekuria@gmail.com>
2015-12-18 18:59:55 -0800

    created our sql tables

--- server/schema.sql
+++ server/schema.sql
@@ -2,11 +2,44 @@ CREATE DATABASE chat;
 
 USE chat;
 
+DROP TABLE IF EXISTS `Messages`;
+
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `User` VARCHAR NULL DEFAULT NULL,
+  `Text` VARCHAR(140) NOT NULL DEFAULT 'NULL',
+  `Room` VARCHAR NULL DEFAULT NULL,
+  `Created at` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE `Users` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `Username` VARCHAR NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+DROP TABLE IF EXISTS `Rooms`;
+    
+CREATE TABLE `Rooms` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `Name` VARCHAR NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+ALTER TABLE `Messages` ADD FOREIGN KEY (User) REFERENCES `Users` (`id`);
+ALTER TABLE `Messages` ADD FOREIGN KEY (Room) REFERENCES `Rooms` (`id`);
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `Users` (`id`,`Username`) VALUES
+-- ('','');
+-- INSERT INTO `Rooms` (`id`,`Name`) VALUES
+-- ('','');
+-- INSERT INTO `Messages` (`id`,`User`,`Text`,`Room`,`Created at`) VALUES
+-- ('','','','','');
 
 
 
